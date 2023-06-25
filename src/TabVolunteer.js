1665e6451884914f997b20b9429e01a67da87223
Farwa <farwa.mubasher@ryerson.ca>
2023-06-24 20:42:13 -0400

    Added Volunteer Experiences

--- src/TabVolunteer.js
+++ src/TabVolunteer.js
@@ -3,8 +3,37 @@ import React from 'react';
 const TabVolunteer = () => {
   return (
     <div>
-      <h2>Volunteer</h2>
-      <p>Here are the list of places I volunteered.</p>
+    <h2>Volunteer and Student Club Experience</h2>
+    <div className="experience">
+        <h3>Vice President Operations</h3>
+        <p>Ryerson Women in Engineering</p>
+        <p>Jul 2020 - Aug 2022 · 2 yrs 2 mos</p>
+        <p>Toronto, Ontario, Canada</p>
+        <ul>
+          <li>Assisting the President to design the company's overall mission, values, and strategic goals.</li>
+          <li>Managing the daily operations and revenue generation of the organization and ensuring its continual growth.</li>
+          <li>Plan, direct, coordinate, and oversee operations activities in the organization, ensuring the development and implementation of efficient operations and cost-effective systems to meet the current and future needs of the organization.</li>
+          <li>Established clear management goals and devised systems to track results for effective decision making social media platforms</li>
+        </ul>
+      </div>
+    <div className="experience">
+        <h3>Director of Events</h3>
+        <p>Magnify Wellness</p>
+        <p>Sep 2020 - Nov 2021 · 1 yr 3 mos</p>
+        <ul>
+          <li>Planning events to help with the mental health of others and promoting the Magnify brand</li>
+          <li>Established clear benchmarks for performance and monitored individual and team progress.</li>
+          <li>Helping with the resolving of any technical problems that might arise especially with all events happening online</li>
+          <li>Promoting Magnify through events and social media platforms</li>
+        </ul>
+      </div>
+      <div className="experience">
+        <h3>Corporate Relationship Assistant</h3>
+        <p>CFES | FCEG / CELC 2021</p>
+        <p>Nov 2019 - Jul 2020 · 9 mos</p>
+        <p>Toronto, Ontario, Canada</p>
+        <p>In this role, I engaged in continuous research in an effort to identify new and potential sponsors or partners. I cultivated and maintained relationships with sponsors and potential partners to ensure continued support. Additionally, I created the sponsorship package for the conference.</p>
+      </div>
     </div>
   );
 };
