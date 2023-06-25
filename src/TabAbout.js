877a6d33ae2ea93899fc6c6b2a238e3cb543bb05
Farwa <farwa.mubasher@ryerson.ca>
2023-06-24 20:28:17 -0400

    Added About Section

--- src/TabAbout.js
+++ src/TabAbout.js
@@ -4,7 +4,11 @@ const TabAbout = () => {
   return (
     <div>
       <h2>About</h2>
-      <p>This is the about section of my personal website.</p>
+      <p>I am a recently graduated student from Toronto Metropolitan University in Computer Engineering. I am interested in exploring the various streams within Computer Engineering. Currently, I am currently working as a Network Intern at Bell. </p>
+ 
+      <p>I love solving challenging problems and presenting solutions. I have worked on several complex challenges within limited time and resources and have successfully tackled them. I have experience in many technical languages such as Java, C, Python and SQL. I have strong skills in communication, organization, teamwork, and leadership. I also have experience in data analysis and building reports in Power Bi. </p>
+ 
+      <p>I am always looking for new challenges and opportunities and am currently searching for a post-graduate job starting September 2023.</p>
     </div>
   );
 };
